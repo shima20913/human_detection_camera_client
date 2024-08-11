4ccedd04fdca5d80134c80c932c47cdc0f8be63d
TakashimaY <yousukes53@gmail.com>
2024-08-11 12:41:17 +0900

    init webcam

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,3 +7,9 @@ load_dotenv()
 IMAGE_DIR = "./images"
 SERVER_URL = os.getenv("SERVER_URL")
 CAPTURE_DELAY = int(os.getenv("CAPTURE_DERAY", 1))
+
+os.mkdirs(IMAGE_DIR, exist_ok=True)
+capt = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("failed open camera")
+    exit()
