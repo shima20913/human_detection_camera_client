24b490c8c52212c28f8d6620f0ee5a17993107cd
TakashimaY <yousukes53@gmail.com>
2024-08-12 16:42:29 +0900

    Added process to delete images after sending them

--- client.py
+++ client.py
@@ -9,7 +9,9 @@ load_dotenv()
 
 IMAGE_DIR = "./images"
 SERVER_URL = os.getenv("SERVER_URL")
-CAPTURE_DELAY = int(os.getenv("CAPTURE_DELAY", 1)) #テスト用に環境変数から読み込めるようにしている
+CAPTURE_DELAY = int(
+    os.getenv("CAPTURE_DELAY", 1)
+)  # テスト用に環境変数から読み込めるようにしている
 
 os.makedirs(IMAGE_DIR, exist_ok=True)
 pygame.init()
@@ -17,15 +19,21 @@ pygame.camera.init()
 camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], (640, 480))
 camera.start()
 
+
 def sendToServer(filename):
     with open(filename, "rb") as file:
-        files = {'file': file}
+        files = {"file": file}
         try:
             response = requests.post(SERVER_URL, files=files)
             response.raise_for_status()
             print("image sent to server successfully:", filename)
         except requests.exceptions.RequestException as e:
             print("Error sending image to server:", e)
+        finally:
+            if os.path.exists(filename):
+                os.remove(filename)
+                print("Image deleted:", filename)
+
 
 def captureAndSend():
     while True:
@@ -35,29 +43,10 @@ def captureAndSend():
         sendToServer(filename)
         time.sleep(CAPTURE_DELAY)
 
+
 if __name__ == "__main__":
     try:
         captureAndSend()
     finally:
         camera.stop()
         pygame.quit()
-
-
-            
-
-
-
-
-
-
-                        
-
-
-
-
-
-
-         
-            
-            
-
